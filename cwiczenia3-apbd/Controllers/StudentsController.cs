87e647e49d7a8da3d1a7331eacd28d96cb2ef846
s16503 <s16503@pjwstk.edu.pl>
2020-03-16 16:30:07 +0100

    dodałem getStudent

---
+++ cwiczenia3-apbd/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cwiczenia3_apbd.Controllers
+{
+
+    [ApiController]     //definiuje zestaw standardowych cech dla api ,, walidować modele
+    [Route("api/students")]
+    public class mojKontroller1Controller : ControllerBase
+    {
+        [HttpGet]       // odpowiada na żądanie GET
+        public string GetStudent() //action methos
+        {
+            return "Jan, Anna, Katarzyna";
+        }
+    }
+}
