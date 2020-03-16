6c0624430a0cc8a1114463a0c1fdc99bea8d3ac9
s16503 <s16503@pjwstk.edu.pl>
2020-03-16 17:28:34 +0100

    dodano createStudent()

--- cwiczenia3-apbd/Controllers/StudentsController.cs
+++ cwiczenia3-apbd/Controllers/StudentsController.cs
@@ -2,19 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using cwiczenia3_apbd.models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace cwiczenia3_apbd.Controllers
 {
 
     [ApiController]     //definiuje zestaw standardowych cech dla api ,, walidować modele
-    [Route("api/students")]
+    [Route("api/students")] 
     public class mojKontroller1Controller : ControllerBase
     {
+
+        //2 q
         [HttpGet]       // odpowiada na żądanie GET
-        public string GetStudent() //action methos
+        public string GetStudents(string orderBy) //action methos
         {
-            return "Jan, Anna, Katarzyna";
+            //var str = string.Format("Ala ma kota{0}",orderBy);
+            // to samo co
+            return $"Jan, Anna, Katarzyna sortowanie ={orderBy}";
         }
+
+
+
+
+        //1 spososób przekazywania danych
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id) //zwraca rezultat z metody action. ...
+        {
+            
+            if (id == 1)
+                return Ok("Jan");
+            else if(id ==2)
+                return Ok("Andrzej");
+
+            return NotFound("not found");
+        }
+
+
+        //3 przekazanie dancyh w cile żądania POST
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+
+            student.Index = $"s{new Random().Next(1, 2000)}";
+            return Ok(student);
+        }
+
+
     }
 }
